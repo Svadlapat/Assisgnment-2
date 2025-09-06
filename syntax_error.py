cb06700979dafc163a4d5066f1f2b9eb2bb4df74
Svadlapat <svadlapatla38567@ucumberlands.edu>
2025-09-06 23:01:08 +0530

    Create syntax_error.py

---
+++ syntax_error.py
@@ -0,0 +1,10 @@
+# Python with a syntax error
+def calculate_sum(arr) # <--- Syntax error: Missing colon
+    total = 0
+    for num in arr:
+        total += num
+    return total
+
+numbers = [1, 2, 3, 4, 5]
+result = calculate_sum(numbers)
+print("Sum in Python:", result)
